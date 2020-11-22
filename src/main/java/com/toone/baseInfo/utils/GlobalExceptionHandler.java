381da8506202f0a2bf114107556c7567514dabf1
huangbh2020 <630761314@qq.com>
2020-11-22 12:03:12 +0800

    首次提交

--- src/main/java/com/toone/baseInfo/utils/GlobalExceptionHandler.java
+++ src/main/java/com/toone/baseInfo/utils/GlobalExceptionHandler.java
@@ -13,32 +13,28 @@ import javax.servlet.http.HttpServletRequest;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private final static  Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     /**
      * 全局捕获异常
      * ExceptionHandler 表示要拦截的异常
-     * @param ex
-     * @return
      */
     @ResponseBody
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity globalExceptionResult(Exception ex){
         logger.info(ex.getMessage());
-        return ResponseUtil.exceptionFail(ex.getMessage().toString());
+        return ResponseUtil.exceptionFail(ex.getMessage());
     }
 
     /**
      * 处理 Exception 异常
-     *
      * @param httpServletRequest httpServletRequest
-     * @param e                  异常
-     * @return
+     * @param e 异常
      */
     @ResponseBody
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity exceptionHandler(HttpServletRequest httpServletRequest, Exception e) {
         logger.error("服务错误:", e);
-        return ResponseUtil.exceptionFail(e.getMessage().toString());
+        return ResponseUtil.exceptionFail(e.getMessage());
     }
 
 }
